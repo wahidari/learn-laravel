08fbac57621aacbb11478b24e3b88065b1265437
wahidari <wahiid.ari@gmail.com>
2019-11-23 01:35:41 +0700

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,7 +6,6 @@
     <div class="row">
         <div class="col-lg-12 text-center">
             <h1 class="mt-5">About Page</h1>
-            <p class="lead">Complete with pre-defined file paths and responsive navigation!</p>
         </div>
     </div>
 </div>

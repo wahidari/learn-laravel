25699907547aa4794b278332ed4d27ff8accee22
wahidari <wahiid.ari@gmail.com>
2019-12-08 00:25:52 +0700

    Update StudentsController.php
    fix namespace error and change query builder to eloquent orm

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+// fixing namespace error
+use App\Student;
 
 class StudentsController extends Controller
 {
@@ -14,10 +16,13 @@ class StudentsController extends Controller
      */
     public function index()
     {
-        //
-        $users = DB::table('students')->get();
+        //Query Builder
+        //$users = DB::table('students')->get();
         // dump($users);
         // var_dump($users);
+
+        //Eloquent ORM
+        $users = Student::all(); 
         return view('data', ['users' => $users]);
     }
 

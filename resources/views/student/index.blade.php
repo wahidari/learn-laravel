b6295fc70a89a81bc92891f9e378b31debb99c5d
wahidari <wahiid.ari@gmail.com>
2019-12-13 00:39:36 +0700

    Update index.blade.php

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -6,21 +6,25 @@
 <div class="container">
     <div class="row">
         <div class="col-6">
-            <h3>Students List :</h3>
-            <a href="/student/create" class="btn my-2 btn-sm btn-success">New</a>
+            <a href="/student/create" class="btn mb-3 btn-sm btn-success">New</a>
             @if (session('status'))
             <div class="alert alert-success">
                 {{ session('status') }}
             </div>
             @endif
-            <ul class="list-group">
-                @foreach($students as $student)
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    {{ $student -> nama }}
-                    <a href="/student/detail/{{ $student -> id }}" class="btn btn-sm btn-primary">Detail</a>
-                </li>
-                @endforeach
-            </ul>
+            <div class="card">
+                <div class="card-header">
+                   <b> Students List : </b>
+                </div>
+                <ul class="list-group list-group-flush">
+                    @foreach($students as $student)
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        {{ $student -> nama }}
+                        <a href="/student/detail/{{ $student -> id }}" class="btn btn-sm btn-primary">Detail</a>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
         </div>
     </div>
 </div>
